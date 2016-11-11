99614a963da04510342e91a9c983f1b5d2bd3cd1
nabadov <nabadov.r@gmail.com>
2016-11-11 13:50:41 +0400

    Changes

---
+++ module1-solution/js/app.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  angular.module('MyApp', [])
+  .controller('DishesController', DishesController);
+
+  DishesController.$inject = ['$scope'];
+
+  function DishesController($scope) {
+    $scope.dishes = "";
+    $scope.submit = function (dishes) {
+      if (dishes == "") {
+        $scope.message = "Please enter data first";
+      }
+      else {
+        var dishesList = dishes.split(',');
+        var count = dishesList.length;
+        if (count <=3) {
+          $scope.message = "Enjoy";
+        }
+        else {
+          $scope.message = "Too much!";
+        }
+      }
+    }
+  }
+
+})();
